409d3e6a9e761faad82682e82c2826c18e7f83eb
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-10-22 19:44:00 +0900

    change dry-run option to print

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ Options:
 {{range .Flags}}  {{.}}
 {{end}}`
 
-const statusTmpl = "@tomo_e {{if .Place}}{{.Place}}で{{else}}どこでもいいので{{end}}{{if .Thing}}{{.Thing}}が{{end}}おいしいお店教えてください！{{if .People}}人数は{{.People}}人です！{{end}}"
+const messageTmpl = "{{if .Place}}{{.Place}}で{{else}}どこでもいいので{{end}}{{if .Thing}}{{.Thing}}が{{end}}おいしいお店教えてください！{{if .People}}人数は{{.People}}人です！{{end}}"
 
 func main() {
 	cli.AppHelpTemplate = helpTemplate[1:]
@@ -36,7 +36,7 @@ func main() {
 		cli.StringFlag{Name: "place, p", Usage: "Set place"},
 		cli.StringFlag{Name: "thing, t", Usage: "Set thing"},
 		cli.IntFlag{Name: "people, n", Usage: "Set number of people"},
-		cli.BoolFlag{Name: "dry-run", Usage: "Show preview"},
+		cli.BoolFlag{Name: "print", Usage: "Show message to stdout"},
 		cli.HelpFlag,
 	}
 
@@ -50,7 +50,7 @@ func main() {
 }
 
 func doMain(c *cli.Context) {
-	tmpl, err := template.New("status").Parse(statusTmpl)
+	tmpl, err := template.New("status").Parse(messageTmpl)
 
 	if err != nil {
 		log.Fatal(err)
@@ -67,14 +67,14 @@ func doMain(c *cli.Context) {
 		log.Fatal(err)
 	}
 
-	status := doc.String()
-	if c.Bool("dry-run") {
-		fmt.Println(status)
+	message := doc.String()
+	if c.Bool("print") {
+		fmt.Println(message)
 		return
 	}
 
 	q := url.Values{}
-	q.Set("status", status)
+	q.Set("status", "@tomo_e "+message)
 
 	cmd := exec.Command("sh", "-c", "open https://twitter.com/intent/tweet?"+q.Encode())
 	cmd.Stdout = os.Stdout
